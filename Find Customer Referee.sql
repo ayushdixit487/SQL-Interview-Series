8bcd8137ded9396eaabef9d98096523a0e0de6ee
Ayush Dixit <ayushdixit661@gmail.com>
2022-06-22 23:16:42 +0530

    Create Find Customer Referee.sql

---
+++ Find Customer Referee.sql
@@ -0,0 +1,29 @@
+/*
+Given a table customer holding customers information and the referee.
++------+------+-----------+
+| id | name | referee_id|
++------+------+-----------+
+| 1 | Will | NULL |
+| 2 | Jane | NULL |
+| 3 | Alex | 2 |
+| 4 | Bill | NULL |
+| 5 | Zack | 1 |
+| 6 | Mark | 2 |
++------+------+-----------+
+Write a query to return the list of customers NOT referred by the person with id '2'.
+
+For the sample data above, the result is:
++------+
+| name |
++------+
+| Will |
+| Jane |
+| Bill |
+| Zack |
++------+
+
+*/
+# Write your MySQL query statement below
+select name
+from customer
+where referee_id is null or referee_id <> 2;
